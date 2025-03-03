b2a63a58ee5655535d8d3c1a96ffd0400efd9522
fakhirula <fakhirula27@gmail.com>
2025-03-04 00:50:13 +0700

    fix: api

--- src/components/Section/DataStatus/index.jsx
+++ src/components/Section/DataStatus/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export function DataLoading() {
   return (
-    <main className="py-6 px-12 space-y-2 bg-gray-300 min-h-screen w-full flex items-center justify-center">
+    <main className="py-6 px-12 space-y-2 min-h-screen w-full flex items-center justify-center">
       <div className="flex items-center space-x-4">
         <Spinner className="h-8 w-8" />
         <div className="text-2xl font-bold text-gray-800">
@@ -16,7 +16,7 @@ export function DataLoading() {
 
 export function DataError({ msg }) {
   return (
-    <main className="py-6 px-12 space-y-2 bg-gray-300 min-h-screen w-full flex items-center justify-center">
+    <main className="py-6 px-12 space-y-2 min-h-screen w-full flex items-center justify-center">
       <div className="flex items-center space-x-4">
         <div className="text-2xl font-bold text-gray-800">
           {msg}..

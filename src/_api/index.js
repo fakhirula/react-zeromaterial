e560da9592e42e8bf6280d155b0900b1102f78a9
fakhirula <fakhirula27@gmail.com>
2025-05-14 21:01:56 +0700

    feat: change url api

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const url = 'https://fakhirul.fajarseptianto.my.id'
+const url = 'https://fakhirul.fajarseptianto.my.id'
 // const url = 'https://api-fakhirul.karyakreasi.id'
-const url = 'http://127.0.0.1:8000'
+// const url = 'http://127.0.0.1:8000'
 
 export const API = axios.create({
   baseURL: `${url}/api`

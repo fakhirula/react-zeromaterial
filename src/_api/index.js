9205e81875876a67cff2eb28bad9747073b728f8
fakhirula <fakhirula27@gmail.com>
2025-05-14 19:42:06 +0700

    feat: change url api

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'http://fakhirul.fajarseptianto.my.id'
+const url = 'https://fakhirul.fajarseptianto.my.id'
 // const url = 'https://api-fakhirul.karyakreasi.id'
 // const url = 'http://127.0.0.1:8000'
 

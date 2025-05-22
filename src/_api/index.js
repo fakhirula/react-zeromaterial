cd78ed73f6148d67739ed379c014f625ec15a7ce
fakhirula <fakhirula27@gmail.com>
2025-05-23 00:32:47 +0700

    add

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url = 'https://fakhirul.fajarseptianto.my.id'
-// const url = 'https://api-fakhirul.karyakreasi.id'
+// const url = 'https://fakhirul.fajarseptianto.my.id'
+const url = 'https://api-fakhirul.karyakreasi.id'
 // const url = 'http://127.0.0.1:8000'
 
 export const API = axios.create({
